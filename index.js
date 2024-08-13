99925ae1a665bc5bdca45f8c9cb9b09ef9abb08b
Iam-abdulahad <ahmmedsumon087@gmail.com>
2024-08-13 17:58:57 +0600

    create a post api to send usr data to the server

--- index.js
+++ index.js
@@ -23,16 +23,31 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+
+    const productCollection = client.db("SumonMoto").collection("products");
+    const usersCollection = client.db("SumonMoto").collection("users");
+
+    app.get("/products", async (req, res) => {
+      const cursor = productCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      console.log("Adding new user:", newUser);
+      const result = await usersCollection.insertOne(newUser);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //     await client.close();
   }
 }
 run().catch(console.dir);
